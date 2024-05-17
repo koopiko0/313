81b4ce93f34e333498b5291abc070e2598712982
koopiko0 <113432175+koopiko0@users.noreply.github.com>
2024-05-17 13:56:58 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,12 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Calculator {
-
-    public static boolean search(String[] arr, String x) {
-        for (String i : arr) {
-            if (i.equals(x)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public static String intToRoman(int num) {
         if (num < 1 || num > 3999) {
             throw new IllegalArgumentException("Number should be in range [1, 3999]");
         }
+
         String[] M = {"", "M", "MM", "MMM"};
         String[] C = {"", "C", "CC", "CCC", "CD", "D",
                 "DC", "DCC", "DCCC", "CM"};
@@ -23,65 +15,91 @@ class Calculator {
         String[] I = {"", "I", "II", "III", "IV", "V",
                 "VI", "VII", "VIII", "IX"};
 
-        return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
+        return M[num / 1000] + C[(num % 1000) / 100]
+                + X[(num % 100) / 10] + I[num % 10];
     }
 
     public static void main(String[] args) {
+        int ans;
         Scanner sc = new Scanner(System.in);
-        System.out.println(intToRoman(18));
         System.out.print("Введите пример: ");
         String primer = sc.nextLine();
 
+        String[] arab = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        String[] rim = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        boolean flag_rim = false;
+
+
+
         try {
             String[] parts = primer.split(" ");
-            String num1 = parts[0];
-            String opr = parts[1];
-            String num2 = parts[2];
+            if ((Arrays.binarySearch(rim, parts[0]) >= 0) && (Arrays.binarySearch(rim, parts[2]) >= 0)) {
+                flag_rim = true;
+            };
+            //Перевод с римских в арабские
+            int a = switch (parts[0]) {
+                case "I" -> 1;
+                case "II" -> 2;
+                case "III" -> 3;
+                case "IV" -> 4;
+                case "V" -> 5;
+                case "VI" -> 6;
+                case "VII" -> 7;
+                case "VIII" -> 8;
+                case "IX" -> 9;
+                case "X" -> 10;
+                default -> Integer.parseInt(parts[0]);
+            };
 
-            int a = get(num1);
-            int b = get(num2);
-            int ans = switch (opr) {
+            int b = switch (parts[2]) {
+                case "I" -> 1;
+                case "II" -> 2;
+                case "III" -> 3;
+                case "IV" -> 4;
+                case "V" -> 5;
+                case "VI" -> 6;
+                case "VII" -> 7;
+                case "VIII" -> 8;
+                case "IX" -> 9;
+                case "X" -> 10;
+                default -> Integer.parseInt(parts[2]);
+            };
+
+            // Операции над числами вывод арабским числом
+            ans = switch (parts[1]) {
                 case "+" -> a + b;
                 case "-" -> a - b;
                 case "*" -> a * b;
                 case "/" -> a / b;
-                default -> 0;
+                default -> throw new Error();
             };
 
-            if ((search(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}, num1) &&
-                    search(new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"}, num2)) ||
-                    (search(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}, num2) &&
-                            search(new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"}, num1))) {
+            String a1 = parts[0];
+            String b1 = parts[2];
+
+            if ((Arrays.binarySearch(arab, a1) >= 0) && (Arrays.binarySearch(rim, b1) >= 0)) { // Выводит индекс
                 throw new Error();
-            } else if ((search(new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"}, num1) &&
-                    search(new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"}, num2))) {
-                System.out.println(intToRoman(ans));
-            } else if ((search(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}, num1) &&
-                    search(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}, num2))) {
+            };
+            if ((Arrays.binarySearch(arab, b1) >= 0) && (Arrays.binarySearch(rim, a1) >= 0)) { // Выводит индекс
+                throw new Error();
+            };
+
+
+
+            if ((a < 1) | (a > 10) | (b < 1) | (b > 10)) {
+                throw new java.lang.Error();
+            }
+
+            if (flag_rim == true) {
+                System.out.println((intToRoman(ans)));
+            } else {
                 System.out.println(ans);
             }
 
-            if (a < 1 || b < 1) {
-                throw new Error();
-            }
-        } catch (Exception e) {
-            throw new Error();
-        }
-    }
 
-    private static int get(String num) {
-        return switch (num) {
-            case "I" -> 1;
-            case "II" -> 2;
-            case "III" -> 3;
-            case "IV" -> 4;
-            case "V" -> 5;
-            case "VI" -> 6;
-            case "VII" -> 7;
-            case "VIII" -> 8;
-            case "IX" -> 9;
-            case "X" -> 10;
-            default -> 0;
-        };
+
+        } catch (Exception e) {
+            throw new java.lang.Error();
+        }
     }
 }
